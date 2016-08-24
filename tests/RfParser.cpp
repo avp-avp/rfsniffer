b6e321e3fe4df170f990c9efe6936e6ce53fd010
Alexey P <alexey@avp.name>
2016-08-24 11:19:17 +0300

    Changes to be committed: 	modified:   ../../libs 	modified:   ../RfParser.cpp 	modified:   ../main.cpp 	new file:   capture-0307-215444.rcf 	new file:   capture-0307-215449.rcf 	new file:   capture-0906-164649.rcf 	new file:   capture-0906-183444.rcf 	new file:   capture-0906-210412.rcf 	modified:   ../../libs (modified content, untracked content)

--- tests/RfParser.cpp
+++ tests/RfParser.cpp
@@ -1,5 +1,7 @@
 
 #include "stdafx.h"
+#include <sys/types.h>
+#include <dirent.h>
 #include "../libs/libutils/logging.h"
 #include "../libs/libutils/Exception.h"
 #include "../libs/librf/RFParser.h"
@@ -11,18 +13,8 @@
 
 #define MAX_SIZE 10000
 
-string DecodeFile(const char *File)
+string DecodeFile(CRFParser* parser, CLog *log, const char *File)
 {
-	CLog *log = CLog::GetLog("Main");
-	CRFParser parser(log);
-	//parser.EnableAnalyzer();
-
-	//parser.AddProtocol(new CRFProtocolRST());
-	//parser.AddProtocol(new CRFProtocolLivolo());
-	//parser.AddProtocol(new CRFProtocolX10());
-	//parser.AddProtocol(new CRFProtocolRaex());
-	parser.AddProtocol("All");
-
 	FILE *f = fopen(File, "rb");
 	if (!f)
 		return "File not found";
@@ -31,7 +23,7 @@ string DecodeFile(const char *File)
 	size_t dataSize = fread(data, sizeof(base_type), MAX_SIZE, f);
 	fclose(f);
 
-	string result = parser.Parse(data, dataSize);
+	string result = parser->Parse(data, dataSize);
 	delete []data;
 	return result;
 }
@@ -42,15 +34,28 @@ typedef pstr_pair *ppstr_pair;
 
 
 static const pstr_pair Tests[] = {
-//	{ "capture-0906-212933.rcf", "" },
-	{ "capture-0906-214352.rcf", "Oregon:type=1d20 id=15 ch=1 t=22.6 h=41" },
-	{ "capture-0906-214234.rcf", "Oregon:type=1d20 id=15 ch=1 t=22.6 h=41" },
-	{ "capture-0906-212618.rcf", "Oregon:type=1d20 id=15 ch=1 t=22.7 h=41" },
-	{ "capture-0906-183444.rcf", "Oregon:type=1d20 id=15 ch=1 t=23.3 h=39" },
-	{ "capture-0906-165011.rcf", "Oregon:type=1d20 id=15 ch=1 t=24.2 h=40" },
-	{ "capture-0906-164649.rcf", "Oregon:type=1d20 id=15 ch=1 t=23.9 h=40" },
-	{ "capture-0706-211823.rcf", "Oregon:type=1d20 id=15 ch=1 t=24.7 h=46" },
-	{ "capture-0906-210412.rcf", "Oregon:type=1d20 id=15 ch=1 t=23.0 h=40" },
+	{ "capture-0307-215444.rcf", "nooLite:flip=1 cmd=4 addr=9a13 fmt=00 crc=76" },
+	{ "capture-0307-215449.rcf", "nooLite:flip=0 cmd=4 addr=9a13 fmt=00 crc=6a" },
+	{ "capture-2706-190620.rcf", "nooLite:flip=0 cmd=21 type=2 t=29.6 h=39 s3=ff bat=0 addr=1492 fmt=07 crc=ec"},
+	{ "capture-2706-143835.rcf", "nooLite:flip=1 cmd=21 type=2 t=30.3 h=43 s3=ff bat=0 addr=1492 fmt=07 crc=d3"},
+	{ "capture-2706-093124.rcf","nooLite:flip=1 cmd=5 addr=9a12 fmt=00 crc=e5" },
+	{ "capture-2706-093129.rcf","nooLite:flip=0 cmd=10 addr=9a12 fmt=00 crc=88" },
+	{ "capture-2706-093217.rcf","nooLite:flip=0 cmd=15 addr=9a11 fmt=00 crc=b4" },
+	{ "capture-2706-093128.rcf","nooLite:flip=1 cmd=5 addr=9a12 fmt=00 crc=e5" },
+	{ "capture-2706-093221.rcf","nooLite:flip=1 cmd=15 addr=9a11 fmt=00 crc=a8" },
+	{ "capture-2706-093205.rcf","nooLite:flip=0 cmd=15 addr=9a13 fmt=00 crc=fb" },
+	//{ "","" },
+	{ "capture-2506-120004.rcf", "nooLite:flip=0 cmd=5 addr=9a13 fmt=00 crc=52"},
+	{ "capture-2506-115944.rcf", "nooLite:flip=1 cmd=5 addr=9a13 fmt=00 crc=4e" },
+	//	{ "capture-0906-212933.rcf", "" },* /
+	{ "capture-0906-214352.rcf", "Oregon:type=1D20 id=15 ch=1 t=22.6 h=41" },
+	{ "capture-0906-214234.rcf", "Oregon:type=1D20 id=15 ch=1 t=22.6 h=41" },
+	{ "capture-0906-212618.rcf", "Oregon:type=1D20 id=15 ch=1 t=22.7 h=41" },
+	{ "capture-0906-183444.rcf", "Oregon:type=1D20 id=15 ch=1 t=23.3 h=39" },
+	{ "capture-0906-165011.rcf", "Oregon:type=1D20 id=15 ch=1 t=24.2 h=40" },
+	{ "capture-0906-164649.rcf", "Oregon:type=1D20 id=15 ch=1 t=23.9 h=40" },
+	{ "capture-0706-211823.rcf", "Oregon:type=1D20 id=15 ch=1 t=24.7 h=46" },
+	{ "capture-0906-210412.rcf", "Oregon:type=1D20 id=15 ch=1 t=23.0 h=40" },
 	
 
 	
@@ -72,35 +77,82 @@ static const pstr_pair Tests[] = {
 	{ "capture-0604-212458.rcf", "Livolo:00110011110100010001000" },
 	{ "capture-0604-212505.rcf", "Livolo:00110011110100010001000" },  
 	{ "capture-0604-212552.rcf", "Livolo:00110011110100010001000" },
-	//*/
+	//* /
 
 	{ "capture-1604-081847.rcf", "Raex:111110000011110111111000001000000011111111111111111111111" },
 	{ "capture-1604-080728.rcf", "Raex:111110000011110111111000001000000011111111111111111111111" },
 	{ "capture-1404-083759.rcf", "Raex:111110000011110111111000001000000011111111111111111111111" },
 	{ "capture-1404-083803.rcf", "Raex:111110000011110111111000001000000011111111111111111111111" },
 	{ "capture-1404-083811.rcf", "Raex:100001000011110111111000010000000011111111111111111111111" },
-//	{ "capture-1404-083816.rcf", "Raex:100001000011110111111000010000000011111111111111111111111" },  // FAIL
-//	{ "capture-1404-083823.rcf", "Raex:100001000011110111111000010000000011111111111111111111111" },  // FAIL
 
-	//*/
-	//	{ "capture-0804-094607.rcf", "?" },
-		{ "capture-0904-091533.rcf", "RST:id=1b10 h=20 t=26.7" },
-		{ "capture-0904-091545.rcf", "RST:id=1b10 h=20 t=26.9" },
+//File: capture-1408-204138.rcf, decoded: RST:id=1b00 h=46 t=14.1
+		{ "capture-1408-204138.rcf", "RST:id=1b00 h=46 t=14.1" },
+
+//File: capture-1004-105819.rcf, decoded: RST:id=1b10 h=82 t=29.1
 		{ "capture-1004-105819.rcf", "RST:id=1b10 h=82 t=29.1" },
-		{ "capture-1004-121901.rcf", "RST:id=1b10 h=34 t=27.2"},
+
+	// Inverted
+	//	{ "capture-0804-094607.rcf", "?" },
+		{ "capture-0904-091533.rcf", ""}, //RST:id=1b10 h=20 t=26.7" },
+		{ "capture-0904-091545.rcf", ""}, //RST:id=1b10 h=20 t=26.9" },
+		{ "capture-1004-121901.rcf", ""}, //RST:id=1b10 h=34 t=27.2"},
+	
 		{"capture-1004-122939.rcf", ""},  //FAILED
 		{"capture-1004-125748.rcf","X10:D2OFF" },
 
-
-		{"capture-1506-164227.rcf", "X10:D2ON"},
-		{"capture-1506-164315.rcf", "??"},
 		//*/
 	NULL
 };
 
+void getAllTestFiles( string path, string_vector &result )
+{
+   DIR* dirFile = opendir( path.c_str() );
+   if ( dirFile ) 
+   {
+      struct dirent* hFile;
+      errno = 0;
+      while (( hFile = readdir( dirFile )) != NULL ) 
+      {
+         if ( !strcmp( hFile->d_name, "."  )) continue;
+         if ( !strcmp( hFile->d_name, ".." )) continue;
+
+         // in linux hidden files all start with '.'
+         bool gIgnoreHidden = true;
+         if ( gIgnoreHidden && ( hFile->d_name[0] == '.' )) continue;
+
+         // dirFile.name is the name of the file. Do whatever string comparison 
+         // you want here. Something like:
+         if ( strstr( hFile->d_name, ".rcf" ))
+         	result.push_back(hFile->d_name);
+      } 
+      closedir( dirFile );
+   }
+}
+
 void RfParserTest()
 {
+	CLog *log = CLog::GetLog("Main");
+	CRFParser parser(log);
+	//parser.EnableAnalyzer();
+
+	//parser.AddProtocol(new CRFProtocolRST());
+	//parser.AddProtocol(new CRFProtocolLivolo());
+	//parser.AddProtocol(new CRFProtocolX10());
+	//parser.AddProtocol(new CRFProtocolRaex());
+	parser.AddProtocol("All");
+
 	ppstr_pair test = Tests;
+
+/*
+	string_vector files;
+	getAllTestFiles(".", files);
+	for_each(string_vector, files, f)
+	{
+		string res = DecodeFile(&parser, log, f->c_str());
+		printf("File: %s, decoded: %s\n", f->c_str(), res.c_str());
+	}
+	return;
+	*/
 
 	while (**test)
 	{
@@ -118,7 +170,7 @@ void RfParserTest()
 		{
 		}
 
-		string res = DecodeFile((*test)[0]);
+		string res = DecodeFile(&parser, log, (string("testfiles/")+(*test)[0]).c_str());
 		if (res != (*test)[1])
 		{
 			printf("Failed! File:%s, result:%s, Expected: %s\n", (*test)[0], res.c_str(), (*test)[1]); 
